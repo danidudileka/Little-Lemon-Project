fb3f320596281f0c5cf99819ae19fb8cb318d7a7
danidudileka <danidudilekaperera@gmail.com>
2024-03-10 18:45:19 +0530

    Menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Nav from "./components/Nav";
 import Main from "./components/Main";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <>
       <Nav />
       <Main />
+      <Menu/>
     </>
   );
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import recipes from "../recipes";
+import Swal from "sweetalert2";
+
+const Menu = () => {
+  const handleOrder = (id) => {
+    console.log(id, "id is clicked");
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, order it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Ordered!",
+          text: "Your order has been made.",
+          icon: "success",
+        });
+      }
+    });
+  };
+  return (
+    <div className="menu-container">
+      <div className="menu-header">
+        <h2>This week specials!</h2>
+        <button>Order Menu</button>
+      </div>
+
+      <div className="cards">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="menu-items">
+            <img src={recipe.image} alt={recipe.title} />
+            <div className="menu-content">
+              <div className="heading">
+                <h5>{recipe.title}</h5>
+                <p>{recipe.price}</p>
+              </div>
+              {recipe.price}
+              <button
+                className="orderbtn"
+                onClick={() => handleOrder(recipe.id)}
+              >
+                Order Now
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
